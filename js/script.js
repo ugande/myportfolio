a753ac56df5904a662f423e71902cdfda68d946e
Daniel Ugande Michael <danielugande@gmail.com>
2024-04-26 17:36:37 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // Smooth scrolling for all anchor links
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+  });
